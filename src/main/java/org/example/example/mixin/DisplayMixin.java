5b42d3638770119692b34e9faf5683dab24c0118
Maheesha <maheeshasithum@gmail.com>
2023-01-01 22:44:14 +0530

    Done

---
+++ src/main/java/org/example/example/mixin/DisplayMixin.java
@@ -0,0 +1,20 @@
+package org.example.example.mixin;
+
+import org.lwjgl.opengl.Display;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Display.class)
+public class DisplayMixin {
+    @Inject(
+            method = "reset",
+            at = @At("HEAD"),
+            cancellable = true,
+            remap = false
+    )
+    private static void displeyInject(CallbackInfo ci){
+        ci.cancel();
+    }
+}
