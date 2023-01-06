ff871cb37f1e5b623ae895ee948bea351f1b19aa
Maheesha <maheeshasithum@gmail.com>
2023-01-06 23:21:49 +0530

    Slander

--- src/main/java/io/github/maheevil/Entrypoint.java
+++ src/main/java/io/github/maheevil/Entrypoint.java
@@ -3,11 +3,12 @@ package io.github.maheevil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+@SuppressWarnings("unused")
 public class Entrypoint
 {
     public static final Logger LOGGER = LogManager.getLogger();
 
     public static void initialize() {
-        LOGGER.info("Initializing");
+        LOGGER.info("NoColourProfileResets - I lied, resetting all your colour profiles");
     }
 }
